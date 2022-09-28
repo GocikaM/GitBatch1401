0225f3cf90dbdff27ddd44f9a45f8c6792442694
unknown <gordanamladovic@gmail.com>
2022-09-28 14:27:31 -0400

    simple basic project

---
+++ src/class03B14/Class03.java
@@ -0,0 +1,11 @@
+package class03B14;
+
+public class Class03 {
+public static void main(String[] args) {
+	
+
+	System.out.println("Hey this is a git class");
+	System.out.println("Hey this is a change in my project");
+	System.out.println("How are u this s new version");
+	
+}}
